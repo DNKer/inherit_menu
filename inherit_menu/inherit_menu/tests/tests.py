80c76b67a453e100647855e7e5e8d0ca2be3e3ea
Dmitry Korepanov' aka DNK <dnker@mail.ru>
2023-05-02 15:44:09 +0500

    added some tests

---
+++ inherit_menu/inherit_menu/tests/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, Client
+
+
+class TestStaticURL(TestCase):
+    """Тестирование главной страницы."""
+
+    def setUp(self):
+        """ Неавторизованый клиент. """
+        self.guest_client = Client()
+
+    def test_home_url_exists_at_desired_location(self):
+        """Страница / доступна любому пользователю."""
+        try:
+            response = self.guest_client.get('/')
+        except Exception as e:
+            assert False, f'''Страница `/` работает неправильно. Ошибка: `{e}`'''
+        if response.status_code in (301, 302):
+            self.assertEqual(
+                        response.status_code,
+                        200,
+                        'Страница `/posts/<post_id>/edit/` не найдена, '
+                        'проверьте этот адрес в *urls.py*'
+        )
